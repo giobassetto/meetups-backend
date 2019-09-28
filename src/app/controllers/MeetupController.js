e6e6b0fdd397dbe5754970e5f4edbe2dd57147de
Giovanni Bassetto <giovannibasseto@gmail.com>
2019-09-28 12:24:15 -0300

    Update de meetups.

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,4 +1,5 @@
 import { parseISO, startOfHour, isBefore } from 'date-fns';
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
@@ -31,6 +32,32 @@ class MeetupController {
     });
 
     return res.json({ meetup, message: 'OK' });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      file_id: Yup.number(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Not permited arguments' });
+    }
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== req.userId) {
+      return res.status(401).json({ error: 'Not authorized' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(401).json('Past date not permited editing');
+    }
+    await meetup.update(req.body);
+
+    return res.json({ meetup });
   }
 
   async destroy(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -14,12 +14,14 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/meetups', MeetupController.store);
 routes.get('/meetups', MeetupController.index);
+routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.destroy);
 
 export default routes;
